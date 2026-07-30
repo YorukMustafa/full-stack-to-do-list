c9742d24b81a3318bddbb2c63df34657d4776ad9
Mustafa Yörük <namel4104@gmail.com>
2026-07-30 16:30:54 +0300

    Fix AppConfig syntax and bean configuration

--- src/main/java/com/mustafayoruk/todolistfullstack/config/AppConfig.java
+++ src/main/java/com/mustafayoruk/todolistfullstack/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.mustafayoruk.todolistfullstack.config;
 
 import com.mustafayoruk.todolistfullstack.repositorys.IUsersRepositorys;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -9,15 +10,13 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class AppConfig {
     private final IUsersRepositorys iUsersRepository;
 
     public AppConfig(IUsersRepositorys iUsersRepository) {
         this.iUsersRepository = iUsersRepository;
-
     }
 
     @Bean
@@ -33,11 +32,9 @@ public class AppConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
-
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(bCryptPasswordEncoder());
-
         return authProvider;
     }
 
